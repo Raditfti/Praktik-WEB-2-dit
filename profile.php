afc726eca4dd9c84fd8ebd6e0c0136c5b86f896f
Raditfti <fadillahradit93@gmail.com>
2025-06-03 18:01:17 +0800

    Initial commit

---
+++ profile.php
@@ -0,0 +1,52 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/IMG_20221228_113111_098.jpg" alt="Dedy Rosyadi" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2310010046</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%"> Radit Fadillah</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Mangkahui , 11 januari 2005</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">081219140478</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">fradit245@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="#" target="_blank" style="text-decoration:none;">
+                                <img src="media sosial/icons8-facebook-logo.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="#" target="_blank" style="text-decoration:none;">
+                                <img src="media sosial/icons8-instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="#" target="_blank" style="text-decoration:none;">
+                                <img src="media sosial/icons8-twitter-logo.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="#" target="_blank">
+                                <img src="media sosial/icons8-linkedin-logo.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
